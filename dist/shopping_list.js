8851d42cbcd89659b1b50abddf79b19bbbb2f667
Stephenlaye2 <32752271+Stephenlaye2@users.noreply.github.com>
2020-08-29 21:56:04 +0100

    Update shopping_list.js

--- dist/shopping_list.js
+++ dist/shopping_list.js
@@ -181,15 +181,15 @@ document.querySelector("#product-list").addEventListener("click", function (e) {
     // Use the text of the first child of the targeted children elements
     Store.removeProduct(firstChildren[0].textContent);
 
-    var c = e.target.parentElement.parentElement.children;
-    const actualPrice = parseFloat(c[2].textContent);
+    let c = e.target.parentElement.parentElement.children[2].textContent;
+//     const actualPrice = parseFloat(c[2].textContent);
 
     Store.calculatePrice();
     Store.countProduct();
 
     // Call showAlert method
     ui.showAlert(
-      `${e.target.parentElement.parentElement.firstElementChild.textContent}: £${actualPrice} successfully removed`,
+      `${e.target.parentElement.parentElement.firstElementChild.textContent}: £${c} successfully removed`,
       "success"
     );
   }
